1efeb78355b00f6ba736b563d2f71acbda650fb3
Din <hurshid.17.97@mail.ru>
2022-12-26 21:18:06 +0500

    dz6

---
+++ dz6.py
@@ -0,0 +1,27 @@
+# Задача: предложить улучшения кода для уже решённых задач (3-5 задача):
+# С помощью использования **лямбд, filter, map, zip, enumerate, list comprehension
+
+# 1. Пример: [1.6, 10.4, 27.84, 3.84, 14.4]
+
+# mile_distances = [1.0, 6.5, 17.4, 2.4, 9]
+# kilometer_distances = list(map(lambda x: x * 1.6, mile_distances))
+# print (kilometer_distances)
+
+
+# 2. Пример: ['вай', 'вай', 'вай', 'вай', 'вай']
+
+# mixed = ['вай', 'просо', 'вай', 'вай', 'просо', 'вай', 'просо', 'просо', 'просо', 'вай']
+# rembo = list(filter(lambda x: x == 'вай', mixed))
+# print (rembo)
+
+
+# 3. Пример: [(1, 'x', None), (2, 'y', True)]
+
+# a = [1,2,3]
+# b = "REM"
+# c = (None, True, False)
+# res = list(zip(a, b, c))
+# print (res)
+ 
+
+
